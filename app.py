f4a194e14fba33ce8d85e2854d6f78a05fd8dadb
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-03-01 15:34:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,60 @@
 import streamlit as st
+import math
 
-st.title("Simple Calculator")
+st.title("Advanced Calculator")
 
 num1 = st.number_input("Enter the first number")
 num2 = st.number_input("Enter the second number")
 
-operation = st.selectbox("Select operation", ["Addition", "Subtraction", "Multiplication", "Division"])
+add_btn = st.button("Addition")
+sub_btn = st.button("Subtraction")
+mul_btn = st.button("Multiplication")
+div_btn = st.button("Division")
+pow_btn = st.button("Power")
+sin_btn = st.button("Sin")
+cos_btn = st.button("Cos")
+tan_btn = st.button("Tan")
 
-if st.button("Calculate"):
-    if operation == "Addition":
+if sin_btn:
+    result = math.sin(num1)
+    st.success(f"The Sin of {num1} is {result:.2f}")
+elif cos_btn:
+    result = math.cos(num1)
+    st.success(f"The Cos of {num1} is {result:.2f}")
+elif tan_btn:
+    result = math.tan(num1)
+    st.success(f"The Tan of {num1} is {result:.2f}")
+else:
+    if add_btn:
         result = num1 + num2
-    elif operation == "Subtraction":
+    elif sub_btn:
         result = num1 - num2
-    elif operation == "Multiplication":
+    elif mul_btn:
         result = num1 * num2
-    elif operation == "Division":
+    elif div_btn:
         if num2 != 0:
             result = num1 / num2
         else:
             st.warning("Division by 0 error. Please enter a non-zero number as divisor.")
             result = "Not defined"
-    st.success(f"Result is {result}")
+    elif pow_btn:
+        result = num1 ** num2
+    else:
+        result = None
 
-    st.write("---")
+    if result is not None:
+        st.success(f"Result is {result}")
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+ 
+st.write("---")
 st.write("created by __")
 st.write("       Shib Kumar Saraf ")
