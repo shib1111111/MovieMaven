52b6127b960108d81b622010659f22b0489cc7a5
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-04-09 15:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from imdb import IMDb
 ia = IMDb()
 
 # Set up the Streamlit app
-st.set_page_config(page_title="IMDb Movie Details", page_icon=":movie_camera:", layout="wide")
 st.title('IMDb Movie Details')
 st.write('Enter a movie title (e.g. "The Godfather")')
 movie_title = st.text_input('', '')
@@ -13,29 +12,28 @@ movie_title = st.text_input('', '')
 # Handle the search button click
 if st.button('Search'):
     # Search for the movie by title
-    movie_results = ia.search_movie(movie_title)
-    if len(movie_results) == 0:
-        st.write("No movies found with that title.")
+    movie = ia.search_movie(movie_title)[0]
+    ia.update(movie)
+
+    # Display the movie details
+    st.write('**Title:**', movie['title'])
+    st.write('**Year:**', movie['year'])
+    st.write('**Rating:**', movie['rating'])
+    st.write('**Genres:**', ', '.join(movie['genres']))
+    if 'plot' in movie:
+        st.write('**Plot:**', movie['plot'][0])
     else:
-        movie = ia.get_movie(movie_results[0].getID())
-        ia.update(movie)
+        st.write('**Plot:** N/A')
 
-        # Display the movie details
-        st.write('**Title:**', movie['title'])
-        st.write('**Year:**', movie['year'])
-        st.write('**Rating:**', movie['rating'])
-        st.write('**Genres:**', ', '.join(movie['genres']))
-        if 'plot' in movie:
-            st.write('**Plot:**', movie['plot'][0])
-        else:
-            st.write('**Plot:** N/A')
+    # Display the top 10 cast members
+    st.write('**Top 10 Cast Members:**')
+    for person in movie['cast'][:10]:
+        st.write(person)
 
-        # Display the top 10 cast members
-        st.write('**Top 10 Cast Members:**')
-        for i, person in enumerate(movie['cast'][:10]):
-            st.write(f'{i+1}. {person}')
-
-        # Display the top 5 recommendations
+    # Display the top 5 recommendations (if available)
+    if 'recommendations' in movie:
         st.write('**Top 5 Recommendations:**')
         for i, recommendation in enumerate(movie['recommendations'][:5]):
             st.write(f'{i+1}. {recommendation["title"]} ({recommendation["year"]})')
+    else:
+        st.write('**Recommendations:** N/A')
