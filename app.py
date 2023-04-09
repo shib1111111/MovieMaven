0a0966e04e5390fd9d2e02589117e9475048ab20
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-04-09 12:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,85 +1,47 @@
-import streamlit as st
 import math
 
-# Setting up the calculator layout
-st.set_page_config(page_title="Calculator", page_icon=":iphone:", layout="wide")
-st.title("Calculator")
+def add(num1, num2):
+    return num1 + num2
 
-col1, col2, col3, col4 = st.beta_columns(4)
+def subtract(num1, num2):
+    return num1 - num2
 
-with col1:
-    num1 = st.number_input("Enter the first number", value=0.0, step=0.01)
+def multiply(num1, num2):
+    return num1 * num2
 
-with col2:
-    num2 = st.number_input("Enter the second number", value=0.0, step=0.01)
+def divide(num1, num2):
+    if num2 == 0:
+        raise ZeroDivisionError("Cannot divide by zero!")
+    return num1 / num2
 
-# Adding buttons for various operations
-add_btn = st.button("+")
-sub_btn = st.button("-")
-mul_btn = st.button("*")
-div_btn = st.button("/")
-mod_btn = st.button("%")
-pow_btn = st.button("^")
-sqrt_btn = st.button("√")
-sin_btn = st.button("sin")
-cos_btn = st.button("cos")
-tan_btn = st.button("tan")
-log_btn = st.button("log")
-exp_btn = st.button("exp")
-clear_btn = st.button("C")
+def percentage(num1, num2):
+    return num1 * (num2 / 100)
 
-# Performing the selected operation
-if sin_btn:
-    result = math.sin(num1)
-    st.success(f"sin({num1}) = {result:.2f}")
-elif cos_btn:
-    result = math.cos(num1)
-    st.success(f"cos({num1}) = {result:.2f}")
-elif tan_btn:
-    result = math.tan(num1)
-    st.success(f"tan({num1}) = {result:.2f}")
-elif sqrt_btn:
-    result = math.sqrt(num1)
-    st.success(f"√({num1}) = {result:.2f}")
-elif log_btn:
-    result = math.log10(num1)
-    st.success(f"log({num1}) = {result:.2f}")
-elif exp_btn:
-    result = math.exp(num1)
-    st.success(f"exp({num1}) = {result:.2f}")
-else:
-    if add_btn:
-        result = num1 + num2
-        st.success(f"{num1} + {num2} = {result:.2f}")
-    elif sub_btn:
-        result = num1 - num2
-        st.success(f"{num1} - {num2} = {result:.2f}")
-    elif mul_btn:
-        result = num1 * num2
-        st.success(f"{num1} * {num2} = {result:.2f}")
-    elif div_btn:
-        if num2 != 0:
-            result = num1 / num2
-            st.success(f"{num1} / {num2} = {result:.2f}")
-        else:
-            st.warning("Division by zero error. Please enter a non-zero number as divisor.")
-            result = "Not defined"
-    elif mod_btn:
-        result = num1 % num2
-        st.success(f"{num1} % {num2} = {result:.2f}")
-    elif pow_btn:
-        result = num1 ** num2
-        st.success(f"{num1} ^ {num2} = {result:.2f}")
+def exponent(num1, num2):
+    return num1 ** num2
+
+def square_root(num):
+    if num < 0:
+        raise ValueError("Cannot compute square root of a negative number!")
+    return math.sqrt(num)
+
+def calculate(operation, num1, num2=None):
+    if operation == "+":
+        return add(num1, num2)
+    elif operation == "-":
+        return subtract(num1, num2)
+    elif operation == "*":
+        return multiply(num1, num2)
+    elif operation == "/":
+        return divide(num1, num2)
+    elif operation == "%":
+        return percentage(num1, num2)
+    elif operation == "^":
+        return exponent(num1, num2)
+    elif operation == "sqrt":
+        return square_root(num1)
     else:
-        result = None
+        raise ValueError("Invalid operation!")
 
-# Handling cases where the result is not defined
-if result is None:
-    st.info("Perform an operation to see the result.")
 
-# Adding a clear button to reset the calculator
-if clear_btn:
-    st.experimental_rerun()
-
-# Importing the CSS file
 st.markdown('<link rel="stylesheet" href="style.css">', unsafe_allow_html=True)
