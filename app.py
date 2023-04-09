4ea669d36bc7da424f0891b1baa004fbaecd5925
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-04-09 12:29:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import streamlit as st
 import math
 
+# Define calculator operations
 def add(num1, num2):
     return num1 + num2
 
@@ -25,23 +27,54 @@ def square_root(num):
         raise ValueError("Cannot compute square root of a negative number!")
     return math.sqrt(num)
 
-def calculate(operation, num1, num2=None):
-    if operation == "+":
-        return add(num1, num2)
-    elif operation == "-":
-        return subtract(num1, num2)
-    elif operation == "*":
-        return multiply(num1, num2)
-    elif operation == "/":
-        return divide(num1, num2)
-    elif operation == "%":
-        return percentage(num1, num2)
-    elif operation == "^":
-        return exponent(num1, num2)
-    elif operation == "sqrt":
-        return square_root(num1)
-    else:
-        raise ValueError("Invalid operation!")
+# Define calculator layout
+st.set_page_config(page_title="Calculator", page_icon=":iphone:", layout="wide")
+st.title("Calculator")
 
+# Import CSS
+st.markdown('<link rel="stylesheet" type="text/css" href="style.css">', unsafe_allow_html=True)
 
-st.markdown('<link rel="stylesheet" href="style.css">', unsafe_allow_html=True)
+button_labels = [
+    ["(", ")", "C", "÷"],
+    ["7", "8", "9", "×"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["±", "0", ".", "=", "%", "^", "√"]
+]
+
+# Define button mapping
+button_map = {
+    "C": "clear",
+    "÷": "/",
+    "×": "*",
+    "^": "**",
+    "√": "square_root",
+    "=": "calculate"
+}
+
+# Generate calculator buttons
+col_width = st.sidebar.get_column_width() // len(button_labels[0])
+for row in button_labels:
+    button_row = st.empty()
+    for label in row:
+        operation = button_map.get(label, label)
+        if operation == "calculate":
+            button = button_row.button(label, key=operation, width=col_width*2)
+        elif operation == "clear":
+            button = button_row.button(label, key=operation, width=col_width)
+        else:
+            button = button_row.button(label, key=operation, width=col_width)
+        st.markdown(f'<style>div.stButton > button[key="{operation}"] {{background-color: #f0f0f0; color: #333333;}}</style>', unsafe_allow_html=True)
+
+# Define input field for calculator
+input_field = st.text_input("Enter calculation", key="input")
+
+# Handle user input and display result
+if button and operation == "clear":
+    input_field.value = ""
+elif button and operation == "calculate":
+    try:
+        result = eval(input_field.value)
+        input_field.value = str(result)
+    except (SyntaxError, ValueError, ZeroDivisionError) as e:
+        input_field.value = str(e)
