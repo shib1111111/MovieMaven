b43b94cf0079c71971e59690bc805c34e8aceed6
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-04-09 12:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,57 @@
 import streamlit as st
 import math
 
-st.set_page_config(page_title="Advanced Calculator", page_icon=":heavy_division_sign:")
+st.set_page_config(page_title="Calculator", page_icon=":iphone:", layout="wide")
 
-st.title("Advanced Calculator")
+st.title("Calculator")
 
-col1, col2 = st.beta_columns([4, 1])
-
+col1, col2 = st.columns(2)
 with col1:
-    num1 = st.number_input("Enter the first number")
-    num2 = st.number_input("Enter the second number")
-
-    add_btn = st.button("+")
-    sub_btn = st.button("-")
-    mul_btn = st.button("*")
-    div_btn = st.button("/")
-    pow_btn = st.button("x^y")
-    sqrt_btn = st.button("sqrt(x)")
-    log_btn = st.button("log(x)")
-
-    sin_btn = st.button("Sin(x)")
-    cos_btn = st.button("Cos(x)")
-    tan_btn = st.button("Tan(x)")
-    clear_btn = st.button("Clear")
-
+    num1 = st.number_input("Enter the first number", value=0.0, step=0.1)
 with col2:
-    st.write("")
-    st.write("Result")
-    st.write("----")
-    if add_btn:
-        result = num1 + num2
-    elif sub_btn:
-        result = num1 - num2
-    elif mul_btn:
-        result = num1 * num2
-    elif div_btn:
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            st.warning("Division by 0 error. Please enter a non-zero number as divisor.")
-            result = "Not defined"
-    elif pow_btn:
-        result = num1 ** num2
-    elif sqrt_btn:
-        if num1 >= 0:
-            result = math.sqrt(num1)
-        else:
-            st.warning("Square root of negative number is not defined.")
-            result = "Not defined"
-    elif log_btn:
-        if num1 > 0 and num2 > 1:
-            result = math.log(num1, num2)
-        else:
-            st.warning("Logarithm is not defined for the given inputs.")
-            result = "Not defined"
-    elif sin_btn:
-        result = math.sin(num1)
-    elif cos_btn:
-        result = math.cos(num1)
-    elif tan_btn:
-        result = math.tan(num1)
-    elif clear_btn:
-        num1 = 0
-        num2 = 0
-        result = 0
+    num2 = st.number_input("Enter the second number", value=0.0, step=0.1)
+
+row1 = st.row_container()
+add_btn, sub_btn, mul_btn, div_btn, pow_btn = row1.button_group(["+", "-", "×", "÷", "xⁿ"])
+row2 = st.row_container()
+sin_btn, cos_btn, tan_btn, exp_btn, log_btn = row2.button_group(["sin", "cos", "tan", "eˣ", "log"])
+
+result = None
+
+if sin_btn:
+    result = math.sin(num1)
+    st.success(f"sin({num1}) = {result:.2f}")
+elif cos_btn:
+    result = math.cos(num1)
+    st.success(f"cos({num1}) = {result:.2f}")
+elif tan_btn:
+    result = math.tan(num1)
+    st.success(f"tan({num1}) = {result:.2f}")
+elif exp_btn:
+    result = math.exp(num1)
+    st.success(f"e^{num1} = {result:.2f}")
+elif log_btn:
+    result = math.log(num1)
+    st.success(f"log({num1}) = {result:.2f}")
+elif add_btn:
+    result = num1 + num2
+    st.success(f"{num1} + {num2} = {result:.2f}")
+elif sub_btn:
+    result = num1 - num2
+    st.success(f"{num1} - {num2} = {result:.2f}")
+elif mul_btn:
+    result = num1 * num2
+    st.success(f"{num1} × {num2} = {result:.2f}")
+elif div_btn:
+    if num2 != 0:
+        result = num1 / num2
+        st.success(f"{num1} ÷ {num2} = {result:.2f}")
     else:
-        result = None
+        st.warning("Division by 0 error. Please enter a non-zero number as divisor.")
+        result = "Not defined"
+elif pow_btn:
+    result = num1 ** num2
+    st.success(f"{num1}^{num2} = {result:.2f}")
 
-    if result is not None:
-        st.write(result)
-
-st.write("")
-st.write("---")
-st.write("created by Shib Kumar Saraf") 
+if result is None:
+    st.info("Please select an operation.")
