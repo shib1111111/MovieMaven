f3b8849fe4c0727e4e765c8121cf8cd4dfa418d9
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-12-17 02:40:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+# app.py
 import streamlit as st
+from movie_search import search_movie
+from styling import add_custom_css
+from signature import display_signature
 from imdb import IMDb
-# Set up the Streamlit app
+
 st.set_page_config(
     page_title="MovieMaven",
     page_icon=":movie_camera:",
@@ -8,180 +12,13 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-# Add custom CSS
-st.markdown(
-    f"""
-    <style>
-        .stButton button, .stTextInput input {{
-            background-color: #2c6db8 !important;
-            border-color: #2c6db8 !important;
-            color: #fff !important;
-        }}
-        .stButton:hover button, .stTextInput:hover input {{
-            background-color: #2c6db8 !important;
-            border-color: #2c6db8 !important;
-            color: #fff !important;
-        }}
-        .stButton:active button {{
-            background-color: #ff4d4d !important;
-            border-color: #ff4d4d !important;
-        }}
-        .stImage {{
-            float: left;
-            margin-right: 1rem;
-        }}
-        .movie-details {{
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: flex-start;
-            height: 100%;
-        }}
-        .movie-details h2 {{
-            margin-top: 0;
-        }}
-        @media (max-width: 768px) {{
-            .stImage {{
-                float: none;
-                margin-right: 0;
-                margin-bottom: 1rem;
-            }}
-            .movie-details {{
-                align-items: center;
-            }}
-        }}
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+add_custom_css()
 
 st.title("MovieMaven")
 st.write('Enter a movie title (e.g. "The Godfather")')
 movie_title = st.text_input("", "")
 
-# Handle the search button click
 if st.button("Search"):
-    with st.spinner("Searching for movie..."):
-        # Initialize the IMDb module
-        ia = IMDb()
+    search_movie(movie_title)
 
-        # Search for the movie by title
-        movies = ia.search_movie(movie_title)
-        if len(movies) > 0:
-            movie = movies[0]
-            ia.update(movie)
-
-            # Display the movie details
-            col1, col2 = st.columns([3, 5])
-            with col1:
-                st.image(movie["full-size cover url"], use_column_width=True)
-            with col2:
-                st.markdown(f"<h2 class='Movie Details'>{movie['title']} ({movie['year']})</h2>",unsafe_allow_html=True)
-                st.write("**Rating:**", movie["rating"])
-                st.write("**Genres:**", ", ".join(movie["genres"]))
-                if "plot" in movie:
-                    st.write("**Plot:**", movie["plot"][0])
-                else:
-                    st.write("**Plot:** N/A")
-
-                # Display the directors
-                directors = movie.get('directors')
-                if directors:
-                    directors_str = [director.get('name') for director in directors]
-                    st.write("**Directors:**", ", ".join(directors_str))
-                else:
-                    st.write("**Directors:** N/A")
-
-                # Display the top 10 cast members
-
-                st.write("**Top 10 Cast Members:**")
-                cast = movie["cast"][:10]
-
-                # Create a formatted string with the name and character played for each cast member
-                formatted_cast = ""
-                for member in cast:
-                    name = member.get('name')
-                    character = member.get('character')
-                    if character:
-                        formatted_cast += f"{name} as {character}\n"
-                    else:
-                        formatted_cast += f"{name}\n"
-
-                # Display the formatted cast list
-                st.code(formatted_cast)
-                # Display the writers
-                writers = movie.get('writers')
-                if writers:
-                    writers_str = [writer.get('name') if writer else '' for writer in writers]
-                    st.write("**Writers:**", ", ".join(writers_str))
-                else:
-                     st.write("**Writers:** N/A")
-
-
-
-                # Display the production companies
-                production_companies = movie.get('production companies')
-                if production_companies:
-                    production_companies_str = [company.get('name') for company in production_companies]
-                    st.write("**Production Companies:**", ", ".join(production_companies_str))
-                else:
-                    st.write("**Production Companies:** N/A")
-
-                # Display the box office gross (if available)
-                box_office = movie.get('box office', {}).get('gross')
-                if box_office:
-                    st.write("**Box Office Gross:**", box_office)
-                else:
-                    st.write("**Box Office Gross:** N/A")
-
-                # Display the budget (if available)
-                budget = movie.get('box office', {}).get('budget')
-                if budget:
-                    st.write("**Budget:**", budget)
-                else:
-                    st.write("**Budget:** N/A")
-
-                # Display recommendations, if available
-                if "recommendations" in movie:
-                    st.write("**Top 5 Recommendations:**")
-                    for i, recommendation in enumerate(movie["recommendations"][:5]):
-                        st.write(
-                            f'{i+1}. {recommendation["title"]} ({recommendation["year"]})'
-                        )
-                else:
-                    st.write("**Recommendations:** N/A") # If no recommendations are available
-
-        else:
-            st.write(f"No movie found with the title '{movie_title}'")
-
-# Define a function to display the signature
-def display_signature():
-    st.markdown(
-        """
-        <style>
-        .signature {
-            font-size: 1rem;
-            font-style: italic;
-            text-align: center;
-            padding: 1rem 0;
-            color: #333;
-            transition: color 0.5s ease-in-out;
-        }
-        .signature:hover {
-            color: #007bff;
-        }
-        </style>
-        """
-        , unsafe_allow_html=True
-    )
-    st.markdown(
-        """
-        <div class="signature">
-        Made with ❤️ by Shib Kumar Saraf
-        </div>
-        """
-        , unsafe_allow_html=True
-    )
-
-# Add the signature to your Streamlit app
 display_signature()
