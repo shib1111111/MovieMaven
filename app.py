00d3ce69b3ac4e1b00534d3d02d4ddcfa89e955b
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-12-29 22:13:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from movie_search import search_movie
 from styling import add_custom_css
 from signature import display_signature
 from imdb import IMDb
-
+from recommedation import generate_movie_recommendations
 st.set_page_config(
     page_title="MovieMaven",
     page_icon=":movie_camera:",
@@ -20,5 +20,6 @@ movie_title = st.text_input("", "")
 
 if st.button("Search"):
     search_movie(movie_title)
+    generate_movie_recommendations(movie_title)
 
 display_signature()
